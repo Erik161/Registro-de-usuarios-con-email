ca73de06dc7d5a8a7d67543f198d74a99caf6c21
Erik Hernandez <erikestuardo_161@hotmail.com>
2018-05-06 13:44:46 -0600

    function new user witch firebase

---
+++ app.js
@@ -0,0 +1,16 @@
+
+//funcion de registro nuevo con firebase
+function registrar(){
+     var email = document.getElementById('email').value;
+     var contrasena = document.getElementById('contrasena').value;
+     firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+    
+}
