911fc1d4b742e936b27974ee08695b76f745a7e5
Erik Hernandez <erikestuardo_161@hotmail.com>
2018-05-08 00:51:11 -0600

    adding function enter and observe

--- app.js
+++ app.js
@@ -8,9 +8,49 @@ function registrar(){
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        console.log(errorCode);
-        console.log(errorMessage);
         // ...
       });
+
+    
+//funcion para que accedan los usuarios registrados
+    function ingreso(){
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // ...
+          });
+    }
+
+    
+    function observador(){
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log('usuario activo');
+                aparecer();
+              // User is signed in.
+              var displayName = user.displayName;
+              var email = user.email;
+              var emailVerified = user.emailVerified;
+              var photoURL = user.photoURL;
+              var isAnonymous = user.isAnonymous;
+              var uid = user.uid;
+              var providerData = user.providerData;
+              // ...
+            } else {
+                console.log('usuario no activo');
+              // User is signed out.
+              // ...
+            }
+          });
+          
+    }
+
+    observador();
+
+    function aparecer(){
+        var contenido = document.getElementById('contenido');
+        contenido.innerHTML = "solo lo ve usuario activo"
+    }
     
 }
